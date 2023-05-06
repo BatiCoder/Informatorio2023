de8d32b2821d29366169a7837a3402622d0fd673
BatiCoder <kos1617@gmail.com>
2023-05-06 19:06:29 -0300

    yeah

--- Informatorio Python/Semana2/DESAFIO_ANALIZADORTEXTO/Desafio_Analizador_Texto.py
+++ Informatorio Python/Semana2/DESAFIO_ANALIZADORTEXTO/Desafio_Analizador_Texto.py
@@ -33,9 +33,10 @@
 
 
 # Se le pide al usuario que ingrese un fragmento de texto cualquiera por teclado.
-texto_usuario = input("\nIngrese un texto cualquiera, por ej un articulo o frase: ")
+texto_usuario = input(
+    "\nIngrese un texto cualquiera, por ej un articulo o frase: ")
 
-# Definir una lista, para las letras ingresadas por teclado
+# Definir una lista, para las letras ingresadas por teclado.
 lista_letras = []
 print("\nA continuacion ingrese 3 letras a eleccion...")
 
@@ -52,9 +53,38 @@ lista_letras = [letra.lower() for letra in lista_letras]
 # Se definde un diccionario para contar la cantidad de veces que aparece cada letra en el texto.
 contador = {}
 
-# Estructura for que recorre
+# Estructura for que recorre los substrings de la lista de letras en el texto.
 for letra in lista_letras:
     contador[letra] = texto_usuario.count(letra)
 
 # Imprimir el contador.
-print(f"\n{contador}")
+print(
+    f"\nAqui se muestra la cantidad de letras que ingresaste, existentes en el texto: {contador}")
+
+# Separar el texto en una lista de palabras.
+palabras = texto_usuario.split()
+# Contar la cantidad de palabras en la lista.
+cantidad_palabras = len(palabras)
+
+# Imprimir la cantidad de palabras en el texto.
+print("\nLa cantidad de palabras en el texto es:", cantidad_palabras)
+
+# Puedes obtener la primera letra de un texto utilizando la indexación con corchetes y el índice 0
+primera_letra = texto_usuario[0]
+# De manera similar, puedes obtener la última letra de un texto utilizando la indexación con corchetes y el índice -1
+segunda_letra = texto_usuario[-1]
+
+# La indexación de una cadena comienza desde 0 y se extiende hasta n-1, donde n es la longitud de la cadena.
+
+# Invertir el texto.
+texto_inverso = ''.join(reversed(texto_usuario))
+
+# Imprimir el texto invertido.
+print("\nAsi queda el texto invertido: ")
+print(f"{texto_inverso}")
+
+# Identificar si la palabra Python existe en el texto, e imprimir por pantalla si lo esta o no.
+if "PYTHON" or "python" in texto_usuario:
+    print("\nLa palabra Python existe en el texto.")
+else:
+    print("\nLa palabra Python no existe en el texto.")
