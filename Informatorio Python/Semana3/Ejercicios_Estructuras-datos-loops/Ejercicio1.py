968a7430f39354da95141b62b7b1523736c2fcd5
BatiCoder <kos1617@gmail.com>
2023-05-06 23:14:45 -0300

    ea

--- Informatorio Python/Semana3/Ejercicios_Estructuras-datos-loops/Ejercicio1.py
+++ Informatorio Python/Semana3/Ejercicios_Estructuras-datos-loops/Ejercicio1.py
@@ -1,5 +1,11 @@
 # 1-Escribe un programa que pida al usuario una palabra y luego imprima cada
 # letra de la palabra en una línea separada.
 
-palabra=input("Ingrese una palabra: ")
+# Pedir al usuario que ingrese una palabra por teclado
+palabra = input("Ingrese una palabra: ")
 
+# Convertir en una lista las letras de la palabra?.
+palabra_separada = list(palabra)
+
+for letra in palabra_separada:
+    print(letra)
