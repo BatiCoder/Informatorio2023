49aa642f96775be26eec01e8be17c15bd1b786e6
BatiCoder <132491162+BatiCoder@users.noreply.github.com>
2023-05-08 17:45:07 +0000

    Manejando los for

--- Informatorio Python/Semana3/Ejercicios_Estructuras-datos-loops/Ejercicio3.py
+++ Informatorio Python/Semana3/Ejercicios_Estructuras-datos-loops/Ejercicio3.py
@@ -1,2 +1,9 @@
 # 3-Escribe un programa que pida al usuario un número y luego imprima la tabla de
 # multiplicar correspondiente a ese número del 1 al 10.
+
+num=int(input("Ingrese un numero entero: "))
+
+for i in range (1,11):
+    producto = num*i
+    print(f"\n{num} x {i} = {producto}")
+    
