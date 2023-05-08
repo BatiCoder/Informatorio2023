fb7873e2842ce1f849eb0279c824b3773dac8cb3
BatiCoder <132491162+BatiCoder@users.noreply.github.com>
2023-05-08 17:52:12 +0000

    haciendole la dominada.

--- Informatorio Python/Semana3/Ejercicios_Estructuras-datos-loops/Ejercicio3.py
+++ Informatorio Python/Semana3/Ejercicios_Estructuras-datos-loops/Ejercicio3.py
@@ -1,8 +1,11 @@
 # 3-Escribe un programa que pida al usuario un número y luego imprima la tabla de
 # multiplicar correspondiente a ese número del 1 al 10.
 
+# Se ingresa un numero entero por pantalla.
 num=int(input("Ingrese un numero entero: "))
 
+# Estructura for que recorre de 1 hasta 10, se multiplica num con el incremento de i y se 
+# guarda en la variable local producto, y en cada iteracion al final se imprime por pantalla.
 for i in range (1,11):
     producto = num*i
     print(f"\n{num} x {i} = {producto}")
--- Informatorio Python/Semana3/Ejercicios_Estructuras-datos-loops/Ejercicio4.py
+++ Informatorio Python/Semana3/Ejercicios_Estructuras-datos-loops/Ejercicio4.py
@@ -1,1 +1,6 @@
 # 4-Escribe un programa que imprima los números pares del 1 al 100.
+
+# Estructura for que va imprimir los numeros pares del 1 al 100.
+for i in range (2,101,2):
+    print(i)
+
