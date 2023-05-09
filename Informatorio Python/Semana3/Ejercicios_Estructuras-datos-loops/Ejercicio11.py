be361cf4554d26f0f2d7eec5fe85bbff259b735d
BatiCoder <132491162+BatiCoder@users.noreply.github.com>
2023-05-09 18:05:27 +0000

    sigo codiando

--- Informatorio Python/Semana3/Ejercicios_Estructuras-datos-loops/Ejercicio11.py
+++ Informatorio Python/Semana3/Ejercicios_Estructuras-datos-loops/Ejercicio11.py
@@ -2,3 +2,13 @@
 # Un factorial es el producto que resulta de multiplicar un número entero positivo
 # dado por todos los enteros inferiores a él hasta el uno. Por ejemplo, el factorial
 # de 4 es 4! = 4 × 3 × 2 × 1 = 24.
+
+num = int(input("Ingrese un numero: "))
+
+factorial = 0
+
+for n in range (1, num):
+    factorial += num*n
+
+print(f"\nEl factorial de {num} es: {factorial}")
+
