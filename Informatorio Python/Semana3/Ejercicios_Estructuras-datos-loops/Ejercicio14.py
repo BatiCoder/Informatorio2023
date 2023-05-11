aae3697614652d66e671c7f034ca0fba791513e1
BatiCoder <kos1617@gmail.com>
2023-05-10 21:35:04 -0300

    nase

---
+++ Informatorio Python/Semana3/Ejercicios_Estructuras-datos-loops/Ejercicio14.py
@@ -0,0 +1,15 @@
+# 14-Escribe un programa que pida al usuario un número y luego imprima un
+# triángulo de números como el siguiente:
+# 1
+# 2 2
+# 3 3 3
+# 4 4 4 4
+# 5 5 5 5 5
+
+num=int(input("Ingrese un numero entero: "))
+
+for i in range(1,num+1):
+    for j in range(i):
+        print(i, end=" ")
+    print()
+    
