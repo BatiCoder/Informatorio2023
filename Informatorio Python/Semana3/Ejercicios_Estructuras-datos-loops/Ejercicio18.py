963a7ca5f036efda8dce33b2489d7666acf6030d
BatiCoder <132491162+BatiCoder@users.noreply.github.com>
2023-05-11 18:36:31 +0000

    listoooooo

--- Informatorio Python/Semana3/Ejercicios_Estructuras-datos-loops/Ejercicio18.py
+++ Informatorio Python/Semana3/Ejercicios_Estructuras-datos-loops/Ejercicio18.py
@@ -4,3 +4,28 @@
 # 2 3
 # 4 5 6
 # 7 8 9 10
+
+n = int(input("Introduce un número: "))
+numero = 1  # inicializar el número en 1
+for i in range(1, n+1):  # iterar sobre cada fila del triángulo
+    for j in range(i):  # iterar sobre cada número de la fila
+        print(numero, end=' ')  # imprimir el número actual
+        numero += 1  # aumentar el número en 1
+    print()  # imprimir un salto de línea al final de cada fila
+
+# Primero, el programa pide al usuario un número que se almacena en la variable n. 
+# Luego, se inicializa el número actual en 1 en la variable numero.
+
+# A continuación, se utiliza un bucle for para iterar sobre cada fila del triángulo, desde 
+# la primera hasta la fila n. Dentro de este bucle, se utiliza otro bucle for para iterar 
+# sobre cada número de la fila actual, desde el primer número hasta el número correspondiente 
+# a la fila actual. En cada iteración de este bucle interno, se imprime el número actual utilizando 
+# la función print() con el argumento end=' ', que indica que se debe imprimir un espacio en lugar 
+# de un salto de línea al final de cada número. Luego, se aumenta el número actual en 1 para imprimir 
+# el siguiente número en la siguiente iteración del bucle interno.
+
+# Después de imprimir todos los números de la fila actual, se imprime un salto de línea utilizando 
+# la función print() sin argumentos. Esto garantiza que cada fila se imprima en una nueva línea.
+
+# Al finalizar la ejecución del bucle externo, se habrán impreso todos los números del triángulo de 
+# acuerdo al patrón indicado.
--- Informatorio Python/Semana3/Ejercicios_Estructuras-datos-loops/Ejercicio19.py
+++ Informatorio Python/Semana3/Ejercicios_Estructuras-datos-loops/Ejercicio19.py
@@ -3,3 +3,29 @@
 # la suma de sus divisores propios (excluyendo el propio número).
 # Los números perfectos son aquellos iguales a la suma de sus divisores: 6 se
 # puede dividir por 1, 2 y 3, y cuando sumas esos números, el resultado es 6
+
+numero = int(input("Introduce un número: "))
+suma_divisores = 0  # inicializar la suma de divisores en 0
+for i in range(1, numero):  # iterar sobre cada posible divisor propio
+    if numero % i == 0:  # si el número es divisible por el divisor actual
+        suma_divisores += i  # añadir el divisor a la suma de divisores
+if suma_divisores == numero:  # si la suma de divisores es igual al número
+    print(numero, "es un número perfecto")
+else:
+    print(numero, "no es un número perfecto")
+
+
+# Primero, el programa pide al usuario un número que se almacena en la variable numero. Luego, se 
+# inicializa la suma de divisores en 0 en la variable suma_divisores.
+
+# A continuación, se utiliza un bucle for para iterar sobre cada posible divisor propio del número 
+# ingresado por el usuario, desde 1 hasta numero-1. Dentro de este bucle, se utiliza una sentencia 
+# condicional if para verificar si el número es divisible por el divisor actual (es decir, si el 
+# resto de la división es 0). Si es así, se añade el divisor actual a la suma de divisores utilizando 
+# el operador +=.
+
+# Al finalizar la ejecución del bucle, se verifica si la suma de divisores es igual al número ingresado 
+# por el usuario utilizando otra sentencia condicional if. Si es así, se imprime un mensaje indicando 
+# que el número es perfecto. Si no, se imprime un mensaje indicando que el número no es perfecto.
+
+# Con este código, puedes determinar si un número ingresado por el usuario es perfecto o no.
