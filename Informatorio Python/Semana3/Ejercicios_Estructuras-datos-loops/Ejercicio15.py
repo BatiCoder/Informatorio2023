d34bc1e802931d72bb15e1bfa4c0a5e1622f7941
BatiCoder <132491162+BatiCoder@users.noreply.github.com>
2023-05-11 16:32:14 +0000

    eaeaeaaeea

--- Informatorio Python/Semana3/Ejercicios_Estructuras-datos-loops/Ejercicio15.py
+++ Informatorio Python/Semana3/Ejercicios_Estructuras-datos-loops/Ejercicio15.py
@@ -4,6 +4,15 @@
 cadena_texto=input("Ingrese un fragmento de texto: ")
 cant_letras={}
 
-for letra in cadena_texto
+for letra in cadena_texto:
+    if letra in cant_letras:
+        cant_letras[letra]+=1
+    else:
+        cant_letras[letra]=1
+
+print("Frecuencias de letras en el texto:")
+for letra, cant_letra in cant_letras.items():
+    print(letra, "->", cant_letra)
+
 
 
