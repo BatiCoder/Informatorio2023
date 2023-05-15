f38040a3e824f9376df70651ec5a9433dc1e1253
BatiCoder <132491162+BatiCoder@users.noreply.github.com>
2023-05-15 18:14:09 +0000

    promedio

--- Informatorio Python/Semana4/Ejercicio9.py
+++ Informatorio Python/Semana4/Ejercicio9.py
@@ -1,3 +1,28 @@
 
 # 9-Crea una función llamada promedio que tome una lista de números como
 # parámetro y devuelva el promedio de esos números.
+
+def promedio(lista_numeros):
+    # Función que calcula el promedio de una lista de números.
+    
+    suma = 0
+    
+    for numero in lista_numeros:
+        # Itera sobre cada número en la lista.
+        
+        suma += numero
+        # Suma el número actual a la variable 'suma'.
+    
+    promedio = suma / len(lista_numeros)
+    # Calcula el promedio dividiendo la suma total entre la cantidad de números en la lista.
+    
+    return promedio
+    # Devuelve el valor del promedio calculado.
+
+# Tambien se puede hacer asi:
+
+# def promedio(lista_numeros):
+#     # Función que calcula el promedio de una lista de números.
+    
+#     return sum(lista_numeros) / len(lista_numeros)
+#     # Calcula la suma de los números en la lista y la divide entre la cantidad de elementos.
