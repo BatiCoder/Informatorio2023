db77f7291bf53a2d76d818dccd04fa139c96b39f
BatiCoder <132491162+BatiCoder@users.noreply.github.com>
2023-05-15 17:34:43 +0000

    funceando

--- Informatorio Python/Semana4/Ejercicio7.py
+++ Informatorio Python/Semana4/Ejercicio7.py
@@ -1,3 +1,15 @@
 
 # 7-Crea una función llamada imprimir_pares que tome un número entero como
 # parámetro y imprima todos los números pares desde 1 hasta ese número.
+
+def imprimir_pares(numero_entero):
+    # Función que imprime los números pares hasta un número dado.
+    
+    for i in range(1, numero_entero + 1):
+        # Itera desde 1 hasta el número ingresado, incluyéndolo.
+        
+        if i % 2 == 0:
+            # Verifica si el número actual es par.
+            
+            print(f"{i} - ", end="")
+            # Imprime el número seguido de un guion y sin salto de línea.
