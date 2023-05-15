56964a069205820133d216e5558ea20a59e1e734
BatiCoder <132491162+BatiCoder@users.noreply.github.com>
2023-05-15 19:26:51 +0000

    vamos shendo

--- Informatorio Python/Semana4/Ejercicio12.py
+++ Informatorio Python/Semana4/Ejercicio12.py
@@ -2,3 +2,13 @@
 # 12-Crea una función llamada convertir_temperatura que tome una temperatura
 # en grados Celsius y la convierta a grados Fahrenheit. La fórmula de conversión
 # es: Fahrenheit = (Celsius * 9/5) + 32.
+
+def convertir_temperatura(temperatura_celsius):
+    # Convierte una temperatura en grados Celsius a grados Fahrenheit.
+
+    # Fórmula de conversión: Fahrenheit = (Celsius * 9/5) + 32
+    grados_fahrenheit = (temperatura_celsius * 9/5) + 32
+
+    # Retorna la temperatura convertida en grados Fahrenheit.
+    return grados_fahrenheit
+
