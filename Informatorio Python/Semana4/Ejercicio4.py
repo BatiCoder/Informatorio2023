867877d652fa21d0c52407631835d3cc762f9d19
BatiCoder <132491162+BatiCoder@users.noreply.github.com>
2023-05-15 14:58:17 +0000

    eaeaea

--- Informatorio Python/Semana4/Ejercicio4.py
+++ Informatorio Python/Semana4/Ejercicio4.py
@@ -3,7 +3,18 @@
 # devuelva True si es capicúa (es decir, si se lee igual de izquierda a derecha que de
 # derecha a izquierda) y False en caso contrario.
 
-def es_capicua(numero):
-    str(numero)
-    numero_inverso=numero[::-1]
+def es_capicua(numero):  
+# Definición de una función llamada "es_capicua" que recibe un parámetro llamado "numero"
     
+    numero_inverso = int(str(numero)[::-1])  
+    # Convierte el número en una cadena, invierte la cadena y lo convierte de nuevo en un entero
+    
+    if numero_inverso == numero:  # Compara el número invertido con el número original
+        print("El numero es capicua.")  # Si son iguales, imprime que el número es capicúa
+    else:
+        print("El numero no es capicua.")  # Si son diferentes, imprime que el número no es capicúa
+
+es_capicua(int(input("Ingrese un numero: ")))
+# Llama a la función "es_capicua" y pasa como argumento el número ingresado por el usuario
+
+
