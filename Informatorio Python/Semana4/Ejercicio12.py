4a9959231e2f4752944b2f8ff0c7feec5be7692f
BatiCoder <132491162+BatiCoder@users.noreply.github.com>
2023-05-16 13:22:00 +0000

    detallando

--- Informatorio Python/Semana4/Ejercicio12.py
+++ Informatorio Python/Semana4/Ejercicio12.py
@@ -12,3 +12,12 @@ def convertir_temperatura(temperatura_celsius):
     # Retorna la temperatura convertida en grados Fahrenheit.
     return grados_fahrenheit
 
+# Ejemplo de uso: Convertir 25 grados Celsius a grados Fahrenheit
+temperatura_fahrenheit = convertir_temperatura(25)
+print(temperatura_fahrenheit)  # Imprime el resultado: 77.0
+
+# La función convertir_temperatura toma una temperatura en grados Celsius como entrada 
+# y aplica la fórmula de conversión para calcular la temperatura equivalente en grados 
+# Fahrenheit. Luego, devuelve la temperatura convertida. En el ejemplo de uso, se convierten 
+# 25 grados Celsius a grados Fahrenheit y se imprime el resultado, que en este caso sería 77.0
+
