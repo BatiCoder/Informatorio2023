7fc5dfd6d2c0fdc34d07ed81bce0f0b987cc0431
BatiCoder <kos1617@gmail.com>
2023-05-16 00:30:43 -0300

    nasheee

--- Informatorio Python/Semana4/Ejercicio13.py
+++ Informatorio Python/Semana4/Ejercicio13.py
@@ -2,3 +2,22 @@
 # 13-Crea una función llamada calcular_descuento que tome dos parámetros:
 # precio y porcentaje_descuento. La función debe calcular y devolver el precio
 # después de aplicar el descuento.
+
+def calcular_descuento(precio, porcentaje_descuento):
+
+    # Calcula el descuento basado en el porcentaje proporcionado
+    descuento = precio * porcentaje_descuento / 100
+
+    # Resta el descuento al precio original para obtener el precio con descuento
+    precio_con_descuento = precio - descuento
+
+    # Devuelve el precio después de aplicar el descuento
+    return precio_con_descuento
+
+
+# Ejemplo de uso de la función
+precio_final = calcular_descuento(100, 20)
+print(precio_final)  # Imprimirá 80
+
+    
+    
--- Informatorio Python/Semana4/Ejercicio14.py
+++ Informatorio Python/Semana4/Ejercicio14.py
@@ -1,3 +1,8 @@
 
 # 14-Crea una función llamada encontrar_maximo que tome una lista de números
 # como parámetro y devuelva el número máximo de la lista.
+
+def encontrar_maximo(lista):
+    return max(lista)
+    
+print(encontrar_maximo([1,2,3,12,23,32,45,70]))
