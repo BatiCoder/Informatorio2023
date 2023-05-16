e7e4b528c6ca4ec846c5c908a539d768b1703ba3
BatiCoder <132491162+BatiCoder@users.noreply.github.com>
2023-05-16 15:44:03 +0000

    documentando

--- Informatorio Python/Semana4/Ejercicio16.py
+++ Informatorio Python/Semana4/Ejercicio16.py
@@ -16,10 +16,21 @@ def eliminar_duplicados(lista):
     # Retorna la lista sin duplicados.
     return lista_sin_duplicados
 
+Resumen:
+
+# La función eliminar_duplicados toma una lista como parámetro y crea una nueva lista sin duplicados. 
+# Se recorre cada elemento de la lista original y, si el elemento no está presente en la lista 
+# sin duplicados, se agrega a esta última. Al final, se retorna la lista sin duplicados.
+
 # Ejemplo de uso.
 lista_original = [1, 2, 3, 3, 4, 2, 5, 1]
 lista_sin_duplicados = eliminar_duplicados(lista_original)
 print(lista_sin_duplicados)  # Imprime la nueva lista sin duplicados: [1, 2, 3, 4, 5].
+
+# Ejemplo de uso:
+# Se utiliza la función eliminar_duplicados con la lista original [1, 2, 3, 3, 4, 2, 5, 1]. El resultado 
+# se almacena en la variable lista_sin_duplicados. Luego, se imprime el contenido de lista_sin_duplicados, 
+# que es la nueva lista sin duplicados: [1, 2, 3, 4, 5].
 
 
 
