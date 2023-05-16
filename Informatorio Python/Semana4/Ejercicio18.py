55ea4d453c1284e5fc68ec11c2e39b53bf8fb085
BatiCoder <132491162+BatiCoder@users.noreply.github.com>
2023-05-16 15:30:40 +0000

    termine, ahora a documentar y leer teoria

--- Informatorio Python/Semana4/Ejercicio18.py
+++ Informatorio Python/Semana4/Ejercicio18.py
@@ -4,20 +4,20 @@
 # alto y el numero mas bajo en la lista.
 
 def calcular_mayor_diferencia(lista_numeros):
-    # Calcula el número mayor y menor en la lista de números
+    # Calcula el número mayor y menor en la lista de números.
     mayor = max(lista_numeros)
     menor = min(lista_numeros)
 
-    # Calcula la diferencia entre el mayor y el menor
+    # Calcula la diferencia entre el mayor y el menor.
     diferencia = mayor - menor
 
-    # Retorna la diferencia calculada
+    # Retorna la diferencia calculada.
     return diferencia
 
 # Ejemplo.
 numeros = [10, 5, 8, 12, 3, 20]
 mayor_diferencia = calcular_mayor_diferencia(numeros)
-print(mayor_diferencia)  # Imprime 17, que es la mayor diferencia entre los números de la lista
+print(mayor_diferencia)  # Imprime 17, que es la mayor diferencia entre los números de la lista.
 
 # En este ejemplo, se pasa la lista de números [10, 5, 8, 12, 3, 20] como argumento a la función 
 # calcular_mayor_diferencia. La función determina el número mayor (20) y el número menor (3) en 
--- Informatorio Python/Semana4/Ejercicio19.py
+++ Informatorio Python/Semana4/Ejercicio19.py
@@ -3,3 +3,26 @@
 # devuelva Bisiesto si el año es bisiesto, o No es Bisiesto en caso contrario. Un año
 # es bisiesto si es divisible por 4, pero no por 100, a menos que también sea
 # divisible por 400.
+
+def es_bisiesto(anio):
+    # Verifica si el año es divisible por 4 y no divisible por 100,
+    # o si es divisible por 400 y también divisible por 100.
+    if (anio % 4 == 0 and anio % 100 != 0) or (anio % 400 == 0 and anio % 100 == 0):
+        return "Es año bisiesto."
+    else:
+        return "No es un año bisiesto."
+
+# Ejemplo.
+anio = int(input("Ingrese un año: "))  # Solicita al usuario ingresar un año.
+sera_bisiesto = es_bisiesto(anio)  # Llama a la función es_bisiesto para determinar si el año es bisiesto.
+
+print(sera_bisiesto)  # Imprime el resultado obtenido, indicando si el año es bisiesto o no.
+
+# En este código, la función es_bisiesto recibe un año como parámetro y verifica si cumple con 
+# las condiciones para ser considerado un año bisiesto. Si el año cumple las condiciones, se 
+# retorna el mensaje "Es año bisiesto". De lo contrario, se retorna el mensaje "No es un año bisiesto".
+# Luego, se solicita al usuario ingresar un año mediante input y se asigna el valor a la variable anio. 
+# Posteriormente, se llama a la función es_bisiesto pasando el año como argumento, y el resultado se 
+# almacena en la variable sera_bisiesto. Finalmente, se imprime el resultado obtenido, indicando si 
+# el año es bisiesto o no.
+
