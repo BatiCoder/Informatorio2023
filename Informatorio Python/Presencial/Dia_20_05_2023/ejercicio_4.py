5c7eb5ad103bab0f2a98eca27b96b75d08fe1ff2
BatiCoder <kos1617@gmail.com>
2023-05-20 16:28:22 -0300

    3finish

--- Informatorio Python/Presencial/Dia_20_05_2023/ejercicio_4.py
+++ Informatorio Python/Presencial/Dia_20_05_2023/ejercicio_4.py
@@ -2,15 +2,20 @@
 """ Crear una funcion que reciba un numero 'x' e imprima los primeros 'x' números de la sucesión de Fibonacci."""
 
 def fibonacci(x):
+    
+    fibonacci = [0, 1]  # Se empieza con los primeros dos números de la secuencia
+    
+    while len(fibonacci) < x:
+        siguiente = fibonacci[-1] + fibonacci[-2]  # Se calcula el siguiente número de la secuencia
+        fibonacci.append(siguiente)  # Se agrega el siguiente número a la lista
+    
+    return fibonacci
 
-    #...
-
-    return #secuencia
 
 # Ejemplo de uso
 numero = int(input("Ingrese la cantidad de números de Fibonacci que desea imprimir: "))
 numeros_fib = fibonacci(numero)
 
-print("La secuencia de Fibonacci de longitud", numero, "es:")
+print("La secuencia de Fibonacci de longitud", numero, "es:\n")
 for num in numeros_fib:
     print(num)
